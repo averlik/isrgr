706552489e85bebdb070b07433ba47b8bb7b039a
Diana <77232880+averlik@users.noreply.github.com>
2023-07-12 16:13:50 +0900

    Добавьте файлы проекта.

---
+++ isrgr/ServerInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace rgz
+{
+    internal static class ServerInfo
+    {
+        private static readonly string host = "localhost";
+        private static readonly string port = "5432";
+        private static readonly string datebase = "isrgr";
+        private static readonly string username = "postgres";
+        private static readonly string password = "12345";
+
+        public static string GetConnectionString()
+        {
+            return
+                $"Host = {host}; Port = {port}; Database = {datebase}; Username = {username}; Password = {password};";
+        }
+    }
+}
